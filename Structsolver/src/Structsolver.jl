460cded1058e8e70b911c8df391bef9373a4936a
SirIsaacNewton <1490530045@qq.com>
2021-05-18 23:21:50 +0800

    add export functions and types

--- Structsolver/src/Structsolver.jl
+++ Structsolver/src/Structsolver.jl
@@ -1,6 +1,13 @@
 module Structsolver
+export
+    Element,Struct,Nodeforce,Concentratedforce,Disturbutionforce,Uniformforce,Support,Load
+    build_A,build_frame,build_truss,
+    forcecurves,internalforcedispsingle,leftsectionforcecurves,
+    isgb,islc,
+    plot_axis,plot_disp,plot_moment,plot_orign,plot_shear,
+    solvestruct,
+    totruss!
 
 include("Structdefine.jl")
 include("Structsolve.jl")
-
 end # module
